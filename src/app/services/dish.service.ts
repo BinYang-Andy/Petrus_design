8cf8f217cb4905247e6a079cafe53ebadf437501
Bin Yang <magic511865789@hotmail.com>
2022-01-01 12:48:27 -0500

    Angular and RxJS Part 1

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import {Dish} from '../shared/dish';
+import { Dish} from '../shared/dish';
 import { DISHES } from '../shared/dishes';
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,16 +14,19 @@ export class DishService {
   constructor() { }
 
 
-  getDishes():Promise<Dish[]>{
-    return Promise.resolve(DISHES);
+  getDishes():Observable<Dish[]>{
+    // return Promise.resolve(DISHES);
+    return of (DISHES).pipe(delay(2000));  // for Rxjs
   }
 
-  getDish(id:string):Promise<Dish>{
-      return Promise.resolve(DISHES.filter((dish)=> (dish.id === id))[0]);
+  getDish(id:string):Observable<Dish>{
+      // return Promise.resolve(DISHES.filter((dish)=> (dish.id === id))[0]);
+      return of (DISHES.filter((dish)=> (dish.id === id))[0]).pipe(delay(2000));// for Rxjs
   }
 
-  getFeaturedDish():Promise<Dish>{
-      return Promise.resolve(DISHES.filter((dish) => dish.featured)[0]);
+  getFeaturedDish():Observable<Dish>{
+      // return Promise.resolve(DISHES.filter((dish) => dish.featured)[0]);
+      return of (DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000));// for Rxjs
   }
 
 }
